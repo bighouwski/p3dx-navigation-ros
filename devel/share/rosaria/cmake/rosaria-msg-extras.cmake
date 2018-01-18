3a3b2fb901d69c20bdf56f7846c4f3de9e984bc9
bighouwski <marco40122@gmail.com>
2018-01-18 20:22:52 +0100

    clean repo

---
+++ devel/share/rosaria/cmake/rosaria-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(rosaria_MESSAGE_FILES "/home/campusbot/dev/roboware_ws_p3dx_navigation/src/rosaria/msg/BumperState.msg")
+set(rosaria_SERVICE_FILES "")
---
+++ devel/share/rosaria/cmake/rosaria-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(rosaria_MSG_INCLUDE_DIRS "/home/campusbot/dev/roboware_ws_p3dx_navigation/src/rosaria/msg")
+set(rosaria_MSG_DEPENDENCIES geometry_msgs;std_msgs)
